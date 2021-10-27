4c9bdfdda1cdabfe6e957a26fb19c66a3a7d7d9e
Jonathan Foote <jf@jonathanfoote.com>
2021-10-27 16:32:31 -0500

    Handle when gtag is blocked

--- templates/analytics.php
+++ templates/analytics.php
@@ -13,11 +13,14 @@
     gtag('js', new Date());
     gtag('config', 'G-PRN1NWC8N6');
     var downloadPatch = function(url) {
+        const callback = () => {document.location = url;};
         gtag('event', 'click', {
             'event_category': 'Patch Download Click',
             'event_label': url,
             'transport_type': 'beacon',
-            'event_callback': function(){document.location = url;}
+            'event_callback': callback
         });
+        // If nothing after a little, don't wait for it... maybe slow connection or maybe gtag blocked by browser.
+        setTimeout(callback, 500)
     }
 </script>
